6d3feb4f55238542ceb0ce5ad9b9ec1caebddac6
bri <284789+b-@users.noreply.github.com>
2023-10-29 22:47:21 -0400

    Update start.go

--- cmd/start/start.go
+++ cmd/start/start.go
@@ -1,6 +1,10 @@
 package start
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/luthermonson/go-proxmox"
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
 )
@@ -8,11 +12,43 @@ import (
 var Command = &cli.Command{
 	Name:   "start",
 	Usage:  "start",
-	Action: stub,
-	Flags:  []cli.Flag{},
+	Action: startVm,
+	Flags:  []cli.Flag{
+		&cli.Uint64Flag{
+			Name:        "vmid",
+			DefaultText: "",
+			FilePath:    "",
+			Usage:       "`VMID` to start",
+			Required:    true,
+			Aliases:     []string{"c"},
+			Action: func(c *cli.Context, vmid uint64) error {
+				if vmid < 100 || vmid > 999999999 {
+					return fmt.Errorf("VM vmid %vmid out of range")
+				}
+				return(nil)
+			},
+		},
+	},
 }
 
-func stub(c *cli.Context) error {
-	logrus.Info("start!")
+func startVm(c *cli.Context) error {
+	credentials := proxmox.Credentials{
+		Username: c.String("pveuser"),
+		Password: c.String("pvepassword"),
+		Realm:    c.String("pverealm"),
+	}
+	vm := proxmox.VirtualMachine{
+		VMID: proxmox.StringOrUint64(c.Uint64("vmid")),
+	}
+
+	client := proxmox.NewClient(c.String("pveurl"),
+		proxmox.WithCredentials(&credentials),
+	)
+
+	task, err := vm.Start(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	logrus.Info(fmt.Sprintf("startvm called! (vm: %#v, task: %#v)", vm, task))
 	return nil
 }
