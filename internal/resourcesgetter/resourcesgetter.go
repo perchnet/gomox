4beb32c2802bc9dad963366ccd822c8b2f85b81a
bri <284789+b-@users.noreply.github.com>
2023-11-05 00:15:19 -0400

    chore: remove dead code

--- internal/resourcesgetter/resourcesgetter.go
+++ internal/resourcesgetter/resourcesgetter.go
@@ -5,29 +5,29 @@ import (
 	"fmt"
 
 	"github.com/luthermonson/go-proxmox"
-)
+	)
 
 func GetVirtualMachineByVMID(vmid uint64, client proxmox.Client, c context.Context) (vm *proxmox.VirtualMachine, err error) {
 
 	cluster, err := client.Cluster(c)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
 	resources, err := cluster.Resources(c, "vm")
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
 	for _, rs := range resources {
 		if rs.VMID == vmid {
 			node, err := client.Node(c, rs.Node)
 			if err != nil {
-				return nil,err
+				return nil, err
 			}
 			vm, err = node.VirtualMachine(c, int(rs.VMID))
 			if err != nil {
-				return nil,err
+				return nil, err
 			}
 		}
 	}
